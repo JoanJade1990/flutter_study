8b4bd238bc42218d99e24a4f2d95f17e8dc3b2f8
chenhuaiyu <215249714@qq.com>
2020-02-17 19:40:29 +0800

    SingleChildScrollView和SliverAppbar的学习

---
+++ flutter_app/lib/slide/singleScrollView.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MySingleSrollView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<String> letters  = List.generate(50, (index) => '很长的数据$index');
+    return Scaffold(
+        appBar: AppBar(title: Text('Single Child Demo'),),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Center(
+            child: Row(
+              children: List.generate(
+                  letters.length,
+                      (index) => Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0,horizontal: 6.0),
+                    child: Text(letters[index], style: TextStyle(fontSize: 18.0)),
+                  )),
+            ),
+          ),
+        )
+    );
+  }
+}
