ab78af777d8a4cd1da16dd2316a5df967607bd61
chenhuaiyu <215249714@qq.com>
2020-03-31 19:26:39 +0800

    尝试编写仿豆瓣app启动页倒计时和底部导航栏

---
+++ flutter_douban/lib/pages/home/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('build HomePage');
+    return Scaffold(
+      body: Center(
+        child: Text('主页'),
+      ),
+    );
+  }
+}
+
