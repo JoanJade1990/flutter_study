afe4aa0d5c8ae129f1f9a127cd5f739ab95dd44a
chenhuaiyu <215249714@qq.com>
2020-04-13 00:34:01 +0800

    尝试编写仿豆瓣app主页推荐和动态数据

--- flutter_douban/lib/constant/constant.dart
+++ flutter_douban/lib/constant/constant.dart
@@ -3,5 +3,12 @@ import 'package:flutter/material.dart';
 class Constant {
   static const String BASE_URL = 'api.douban.com';
 
+  static const double MARGIN_LEFT = 13.0;
+  static const double MARGIN_RIGHT = 13.0;
   static const String ASSETS_IMG = 'assets/images/';
+
+  static String URL_MP4_DEMO_0 = 'http://vt1.doubanio.com/201902111139/0c06a85c600b915d8c9cbdbbaf06ba9f/view/movie/M/302420330.mp4';
+
+  static String URL_MP4_DEMO_1 = 'http://vt1.doubanio.com/201903032315/702b9ad25c0da91e1c693e5e4dc5a86e/view/movie/M/302430864.mp4';
+
 }
---
+++ flutter_douban/lib/router.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutterdouban/pages/container_page.dart';
+import 'package:flutterdouban/pages/web_view_page.dart';
+
+
+class Router {
+  static const homePage = 'app://';
+  static const detailPage = 'app://DetailPage';
+  static const playListPage = 'app://VideosPlayPage';
+  static const searchPage = 'app://SearchPage';
+  static const photoHero = 'app://PhotoHero';
+  static const personDetailPage = 'app://PersonDetailPage';
+
+//  Widget _router(String url, dynamic params) {
+//    String pageId = _pageIdMap[url];
+//    return _getPage(pageId, params);
+//  }
+//
+//  Map<String, dynamic> _pageIdMap = <String, dynamic>{
+//    'app/': 'ContainerPageWidget',
+//    detailPage: 'DetailPage',
+//  };
+
+  Widget _getPage(String url, dynamic params) {
+    if (url.startsWith('https://') || url.startsWith('http://')) {
+      return WebViewPage(url, params: params);
+    } else {
+      switch (url) {
+        case homePage:
+          return ContainerPage();
+      }
+    }
+    return null;
+  }
+
+//
+//  void push(BuildContext context, String url, dynamic params) {
+//    Navigator.push(context, MaterialPageRoute(builder: (context) {
+//      return _getPage(url, params);
+//    }));
+//  }
+
+  Router.pushNoParams(BuildContext context, String url) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return _getPage(url, null);
+    }));
+  }
+
+  Router.push(BuildContext context, String url, dynamic params) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return _getPage(url, params);
+    }));
+  }
+}
